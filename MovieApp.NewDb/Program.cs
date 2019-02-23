f3b98e15bc0724e24bb4916cd17758a6b8bf7bb5
janetuw <nijanet123@gmail.com>
2019-02-23 17:34:12 +0000

    first commit

---
+++ MovieApp.NewDb/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MovieApp.NewDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieContext())
+            {
+                Console.WriteLine("enter Movie title");
+                string title = Console.ReadLine();
+                Console.WriteLine("enter Movie release year");
+                string year = Console.ReadLine();
+                Console.WriteLine("enter Movie category");
+                string serie = Console.ReadLine();
+                db.Movies.Add(new Movietbl { title = title});
+                db.Movies.Add(new Movietbl { year = year });
+                db.Movies.Add(new Movietbl { category_name = "serie" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All Movies in database:");
+                foreach (var Movietbl in db.Movies)
+                {
+
+
+                    Console.WriteLine("  {0}  {1}  {2}", Movietbl.title , Movietbl.year , Movietbl.category_name);
+
+                }
+            }
+        }
+    }
+}
